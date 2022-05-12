6390feaa866538cb51b557839e8b75562b67dd66
Amarjit <amarjitmangarhia@gmail.com>
2022-05-12 15:55:58 +0530

    backend logic changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import React, { useRef, useState } from "react";
+
 import "./App.css";
 
+let randomNumber = Math.trunc(Math.random() * 21);
+
 function App() {
+  const input = useRef();
+
+  const [guessNumber, setGuessNumber] = useState("?");
+  const [startGuessing, setStartGuessing] = useState("Start Guessing...");
+
+  const onClickHandler = () => {
+    let inputValue = input.current.value;
+    let convertToInt = parseInt(inputValue);
+
+    if (randomNumber === convertToInt) {
+      console.log("matched");
+      setGuessNumber(randomNumber);
+      setStartGuessing("Matched!");
+    } else if (randomNumber < convertToInt) {
+      setStartGuessing("Too High!");
+      console.log(randomNumber);
+    } else if (randomNumber > convertToInt) {
+      setStartGuessing("Too Low!");
+      console.log(randomNumber);
+    }
+  };
+
   return (
     <div className="container">
       <div className="header">
@@ -13,20 +39,27 @@ function App() {
         </div>
         <div className="guess-number">
           <div className="border"></div>
-          <div className="number">?</div>
+          <div className="number">{guessNumber}</div>
         </div>
       </div>
 
       <div className="main">
         <div className="main-section">
           <div className="main-left">
-            <input type="number" defaultValue="0" className="input" />
-            <button className="main-button">Check!</button>
+            <input
+              ref={input}
+              type="number"
+              defaultValue="0"
+              className="input"
+            />
+            <button onClick={onClickHandler} className="main-button">
+              Check!
+            </button>
           </div>
           <div className="main-right">
             <div className="content">
               <div className="start-guessing-text">
-                <p className="main-guess">Start Guessing...</p>
+                <p className="main-guess">{startGuessing}</p>
               </div>
               <div className="main-score">
                 <div className="score">
