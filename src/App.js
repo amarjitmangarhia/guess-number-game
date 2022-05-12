ada9b90a7fe3737a909e2495121a355679e6cf43
Amarjit <amarjitmangarhia@gmail.com>
2022-05-12 14:25:04 +0530

    first upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <div className="button-between">
+          <button className="again-button">Again!</button>
+          <p className="between">"Between 1 and 20"</p>
+        </div>
+        <div className="title">
+          <h1 className="guess-h1">Guess My Number!</h1>
+        </div>
+        <div className="guess-number">
+          <div className="border"></div>
+          <div className="number">?</div>
+        </div>
+      </div>
+
+      <div className="main">
+        <div className="main-section">
+          <div className="main-left">
+            <input type="number" defaultValue="0" className="input" />
+            <button className="main-button">Check!</button>
+          </div>
+          <div className="main-right">
+            <div className="content">
+              <div className="start-guessing-text">
+                <p className="main-guess">Start Guessing...</p>
+              </div>
+              <div className="main-score">
+                <div className="score">
+                  <p>Score: 20</p>
+                </div>
+                <div className="high-score">
+                  <p>Highscore: 0</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
